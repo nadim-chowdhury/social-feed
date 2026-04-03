d55c23e3da1b4da7339667181fec6b99e3618d5f
nadim-chowdhury <nadim-chowdhury@outlook.com>
2026-04-03 19:49:20 +0600

    feat: create FeedPageShell component to layout the main feed interface

--- frontend/components/feed/FeedPageShell.tsx
+++ frontend/components/feed/FeedPageShell.tsx
@@ -15,8 +15,8 @@ export function FeedPageShell() {
       {/* <FeedThemeToggle /> */}
 
       <main className="mx-auto max-w-[1320px] px-4 pb-24 pt-4 lg:px-6 lg:pb-10 lg:pt-6">
-        <div className="grid grid-cols-1 gap-6 lg:grid-cols-12">
-          <div className="lg:col-span-3">
+        <div className="grid grid-cols-1 gap-6 lg:grid-cols-12 lg:items-start">
+          <div className="lg:col-span-3 lg:sticky lg:top-[90px] lg:h-[calc(100vh-100px)] lg:overflow-y-auto [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden lg:pb-4">
             <FeedLeftSidebar />
           </div>
           <div className="lg:col-span-6">
@@ -26,7 +26,7 @@ export function FeedPageShell() {
               <FeedPostCard key={post.id} post={post} />
             ))}
           </div>
-          <div className="hidden lg:col-span-3 lg:block">
+          <div className="hidden lg:col-span-3 lg:block lg:sticky lg:top-[90px] lg:h-[calc(100vh-100px)] lg:overflow-y-auto [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden lg:pb-4">
             <FeedRightSidebar />
           </div>
         </div>
