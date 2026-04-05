2a799ece5d125343f74ef3e4bcd15babeb595d4c
nadim-chowdhury <nadim-chowdhury@outlook.com>
2026-04-05 14:19:03 +0600

    feat: implement AuthHydrator component to manage session state and user authentication synchronization

--- frontend/components/auth/AuthHydrator.tsx
+++ frontend/components/auth/AuthHydrator.tsx
@@ -2,13 +2,15 @@
 
 import { useGetMeQuery } from "@/services/authApi";
 import { RootState } from "@/store";
-import { setCredentials } from "@/store/slices/authSlice";
+import { logout, setCredentials } from "@/store/slices/authSlice";
 import { GlobalAuthHydratorProps } from "@/types/auth";
+import { useRouter } from "next/navigation";
 import { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 export function AuthHydrator({ token, children }: GlobalAuthHydratorProps) {
   const dispatch = useDispatch();
+  const router = useRouter();
   const hydrated = useRef(false);
 
   const authUser = useSelector((state: RootState) => state.auth.user);
@@ -18,27 +20,22 @@ export function AuthHydrator({ token, children }: GlobalAuthHydratorProps) {
     hydrated.current = true;
   }
 
-  if (!token)
-    return (
-      <div className="text-center p-10 font-bold text-red-500">
-        Security Fault: Missing Token
-      </div>
-    );
-
-  const { data: user, isSuccess } = useGetMeQuery(undefined, {
+  const {
+    data: user,
+    isSuccess,
+    isError,
+  } = useGetMeQuery(undefined, {
     skip: !token,
   });
 
   useEffect(() => {
-    if (isSuccess && user) {
-      dispatch(
-        setCredentials({
-          user,
-          token: token,
-        }),
-      );
+    if (isError || !token) {
+      dispatch(logout());
+      router.replace("/login");
+    } else if (isSuccess && user) {
+      dispatch(setCredentials({ user, token: token }));
     }
-  }, [isSuccess, user, token, dispatch]);
+  }, [isSuccess, isError, user, token, dispatch, router]);
 
   if (!authUser)
     return (
